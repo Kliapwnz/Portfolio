2ae53be1e095bae2174c216d45b129a8a2d2a1fa
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-24 15:47:13 +0300

    a href

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -4,7 +4,11 @@ import s from "./Nav.module.css";
 export const Nav = () => {
     return (
         <div className={s.nav}>
-
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
         </div>
     );
 };
