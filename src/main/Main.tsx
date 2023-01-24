4660e2afcbfff15dacf0a74ed5e8cb903a3b4648
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-24 17:08:09 +0300

    container

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,12 +5,14 @@ import s from "./Main.module.css"
 export const Main = () => {
     return (
         <div className={s.mainBlock}>
-            <div className={s.greetings}>
-                <span>Hi There</span>
-                <h1>I am Evgeniy Klimovich</h1>
-                <p>Frontend Developer</p>
+            <div className={s.container}>
+                <div className={s.greetings}>
+                    <span>Hi There</span>
+                    <h1>I am Evgeniy Klimovich</h1>
+                    <p>Frontend Developer</p>
+                </div>
+                <div className={s.photo}></div>
             </div>
-            <div className={s.photo}></div>
         </div>
     );
 };
