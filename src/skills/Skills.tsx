93489e3caba9b467341cae18b0b1cf1c4c12957b
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-24 17:39:21 +0300

    Skills.tsx

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -5,7 +5,10 @@ import sc from "../common/styles/Container.module.css"
 export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
-            <div className={sc.container}></div>
+            <div className={`${sc.container} ${s.skillsContainer}`}>
+                <h2>Skills</h2>
+                <div className={s.skills}></div>
+            </div>
 
         </div>
     );
