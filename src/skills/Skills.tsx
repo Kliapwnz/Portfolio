3a1a0c4573c095440c1b70635e82c6598a6cd970
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-24 17:31:59 +0300

    Skills.tsx

---
+++ src/skills/Skills.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from "./Skills.module.css"
+import sc from "../common/styles/Container.module.css"
+
+export const Skills = () => {
+    return (
+        <div className={s.skillsBlock}>
+            <div className={sc.container}></div>
+
+        </div>
+    );
+};
+
