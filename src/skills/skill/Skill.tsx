77f56d9b7c1ad624a66f60d6b44fd3b24c537004
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-24 17:56:05 +0300

    Skill.tsx

--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from "./Skill.module.css"
 
-export const Skill = () => {
+type PropsType ={
+    title:string
+}
+
+export const Skill = (props:PropsType) => {
     return (
         <div className={s.skill}>
-
+            <div className={s.icon}></div>
+            <h3>{props.title}</h3>
         </div>
     );
 };
